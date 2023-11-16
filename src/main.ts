0251e42eb400926e17313f8815f690bedfaa64e5
Oscar <cyberskely@gmail.com>
2023-11-16 10:54:21 +0000

    Added basic checks for opposite and same sides

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,82 @@
-let sideLength = 5;
+function calculation(): number {
 
-// -------------
-// Create square
-// -------------
+    let sideLength: number = 5;
+    let probability: number = 0;
 
-let topArray = new Array(sideLength).fill(0);
-let leftArray = new Array(sideLength).fill(0);
-let rightArray = new Array(sideLength).fill(0);
-let bottomArray = new Array(sideLength).fill(0);
+    // -------------
+    // Create square
+    // -------------
 
-let square = [topArray, leftArray, rightArray, bottomArray]
+    let topArray: Array<number> = new Array(sideLength).fill(0);
+    let leftArray: Array<number> = new Array(sideLength).fill(0);
+    let bottomArray: Array<number> = new Array(sideLength).fill(0);
+    let rightArray: Array<number> = new Array(sideLength).fill(0);
 
-// --------------------
-// Randomly pick points
-// --------------------
+    let square: Array<Array<number>> = [topArray, leftArray, bottomArray, rightArray]
 
-let rand1 = Math.floor(Math.random() * sideLength)
-let rand2 = Math.floor(Math.random() * sideLength)
+    // --------------------
+    // Randomly pick points
+    // --------------------
 
-while (rand1 == rand2) {
-    rand2 = Math.floor(Math.random() * sideLength)
+    let rand1 = Math.floor(Math.random() * sideLength)
+    let rand2 = Math.floor(Math.random() * sideLength)
+
+    while (rand1 == rand2) {
+        rand2 = Math.floor(Math.random() * sideLength)
+    }
+
+    square[Math.floor(Math.random() * 4)][rand1] = 1
+    square[Math.floor(Math.random() * 4)][rand2] = 1
+
+    // ------------------------------------
+    // Check if both items are on same side
+    // ------------------------------------
+
+    let side1 = -1
+    let side2 = -1
+
+    for (let i = 0; i < square.length; i++) {
+
+        console.log(square[i])
+
+        let countOnSide = 0
+
+        for(let j = 0; j < square[i].length; j++) {
+
+            if(square[i][j] == 1) {
+                countOnSide += 1
+            }
+            
+        }
+
+        if (countOnSide == 2) {
+            probability = 0
+            return probability
+        }
+        
+        // -----------------
+        // Log side of items
+        // -----------------
+
+        else if (countOnSide == 1) {
+            
+            if(side1 == -1) {
+                side1 = i
+            } else {
+                side2 = i
+            }
+        }
+        
+    }
+
+    // ----------------------------------------
+    // Check if both items are on oposite sides
+    // ----------------------------------------
+
+    if(side1 - side2 == 2 || side2 - side1 == 2) {
+        probability = 1
+        return probability
+    }
+
+    return -1
 }
-
-square[Math.floor(Math.random() * 4)][rand1] = 1
-square[Math.floor(Math.random() * 4)][rand2] = 1
-console.log(square)
